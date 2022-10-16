1e9ebff56aed08da6a65dd9db3e1e175733a34f1
Ngoc Tran <tranngoc897@gmail.com>
2022-10-16 10:42:42 +0700

    Test jenkins

--- au-service/src/main/java/vn/com/ibss/au/AuServiceApplication.java
+++ au-service/src/main/java/vn/com/ibss/au/AuServiceApplication.java
@@ -25,6 +25,7 @@ public class AuServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuServiceApplication.class, args);
+        System.out.printf("Demo Jenkins");
     }
 
 }
